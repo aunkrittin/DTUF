a13dbe7cee2218a0d71f08a9a6cd56ccbcd191ca
icutmyhair_ <macchiatolive@gmail.com>
2022-07-27 05:36:17 +0700

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p>This is an early-access. The website may bug pls contact me.</p>
+      <p>&copy; Copyright 2022</p>
+    </div>
+  );
+}
+
+export default Footer;
