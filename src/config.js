773c9332f79ef3ef3d63f80fc23a9f65b5168d27
icutmyhair_ <macchiatolive@gmail.com>
2022-08-01 06:39:31 +0700

    add(firebase): add config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+// import firebase from "firebase/app";
+// import "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyBme01MIh7uMD_ahKH3TiZVlVajz06lvFA",
+  authDomain: "react-auth-f1deb.firebaseapp.com",
+  projectId: "react-auth-f1deb",
+  storageBucket: "react-auth-f1deb.appspot.com",
+  messagingSenderId: "847233287551",
+  appId: "1:847233287551:web:7522c96577e1d7cbebc64a",
+});
+
+export default firebaseConfig;
