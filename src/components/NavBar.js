df95ade748a4b580776eb52b703173c67343882d
icutmyhair_ <macchiatolive@gmail.com>
2022-08-01 06:39:06 +0700

    update(navbar): new navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,65 @@
+import React, { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { AuthContext } from "./Auth";
+import firebaseConfig from "../config";
 // import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar() {
+  const { currentUser } = useContext(AuthContext);
+  // if (!currentUser) {
+  //   return <Navigate to="/login" />;
+  // }
+
   return (
-    <Navbar bg="dark" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">Don't turn your face!!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link className="nav-link" href="/student">
-              Student
-            </Nav.Link>
-            <Nav.Link className="nav-link" href="/teacher">
-              Teacher
-            </Nav.Link>
-            <Nav.Link className="nav-link" href="/login">
-              Login
-            </Nav.Link>
-            <Nav.Link className="nav-link" href="/register">
-              Register
-            </Nav.Link>
+    <>
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">Don't turn your face!!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {currentUser ? (
+              <Nav className="me-auto">
+                <Nav.Link className="nav-link" href="/student">
+                  Student
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="/teacher">
+                  Teacher
+                </Nav.Link>
+                <Nav.Link
+                  className="nav-link"
+                  onClick={() => firebaseConfig.auth().signOut()}
+                >
+                  Logout
+                </Nav.Link>
+              </Nav>
+            ) : (
+              <Nav className="me-auto">
+                <Nav.Link className="nav-link" href="/login">
+                  Log In
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="/signup">
+                  Sign Up
+                </Nav.Link>
+              </Nav>
+            )}
+
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
