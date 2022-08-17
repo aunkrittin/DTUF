0a0fc964d36cac22357d22a3ce588fec51ea3944
icutmyhair_ <macchiatolive@gmail.com>
2022-08-18 05:08:15 +0700

    add detecting when invisible

--- src/components/Exam.js
+++ src/components/Exam.js
@@ -7,6 +7,7 @@ import { doc, setDoc, arrayUnion, getDoc } from "firebase/firestore";
 import { getFirestore } from "@firebase/firestore";
 import { useParams, Navigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import PageVisibility from "react-page-visibility";
 const db = getFirestore(firebaseConfig);
 
 function Exam(props) {
@@ -14,6 +15,92 @@ function Exam(props) {
   const { handle } = useParams();
   const roomId = handle;
   const [finihed, setFinished] = useState();
+  const [gformLink, setgformLink] = useState("");
+  const [timeDisplay, setTimeDisplay] = useState("00:00:00");
+
+  const handleVisibilityChange = async (isVisible) => {
+    // console.log(isVisible);
+    if (isVisible) {
+      const switchingTab = doc(
+        db,
+        `rooms/${roomId}/students_join_room`,
+        `${studentName}`
+      );
+      await setDoc(
+        switchingTab,
+        {
+          activities: arrayUnion({
+            action: "visible",
+            timestamp: new Date(),
+          }),
+        },
+        { merge: true }
+      );
+      // console.log("you in page");
+    } else {
+      const switchingTab = doc(
+        db,
+        `rooms/${roomId}/students_join_room`,
+        `${studentName}`
+      );
+      await setDoc(
+        switchingTab,
+        {
+          activities: arrayUnion({
+            action: "hidden",
+            timestamp: new Date(),
+          }),
+        },
+        { merge: true }
+      );
+      // console.log("sfgsdfdf");
+    }
+  };
+
+  useEffect(() => {
+    const startCapture = async (displayMediaOptions) => {
+      let captureStream = null;
+
+      try {
+        captureStream = await navigator.mediaDevices.getDisplayMedia(
+          displayMediaOptions
+        );
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+      return captureStream;
+    };
+
+    startCapture();
+    getDoc(doc(db, "rooms", roomId)).then((data) => {
+      const roomData = data.data();
+
+      // console.log(data.data());
+      // setTimeCount(data.data());
+      setgformLink(roomData.gformLink);
+
+      let seconds = roomData.timeDuration * 60; // แปลงนาทีเป็น วินาที
+      const interval = setInterval(() => {
+        // console.log("This will run every second!");
+        let hours = Math.floor(seconds / 3600);
+        let minutes = Math.floor((seconds % 3600) / 60);
+        let sec = Math.floor((seconds % 3600) % 60);
+
+        // console.log(`${hours} : ${minutes} : ${sec}`);
+        seconds = seconds - 1;
+        setTimeDisplay(`${hours} : ${minutes} : ${sec}`);
+        // setTimeCount((timeCount) => seconds - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    });
+
+    // getRoomData();
+  }, []);
+
+  //const dataDoc =  getDoc(doc(db, "rooms", roomId));
+  //   // setRoomdata(dataDoc.data());
+  //   console.log(dataDoc);
+  // };
 
   const finishTest = async () => {
     Swal.fire({
@@ -43,21 +130,23 @@ function Exam(props) {
           },
           { merge: true }
         );
+        setFinished("Hello");
       }
-      setFinished("Hello");
     });
   };
 
   return (
     <>
+      <PageVisibility onChange={handleVisibilityChange}></PageVisibility>
       {!finihed ? (
         <div className="home-body mt-0">
+          <h1 className="clock">{timeDisplay}</h1>
           <Container>
             <Row className="home-main-row">
               <Col>
                 <Card className="">
                   <Card.Body>
-                    <iframe src="" width="100%" height="750"></iframe>
+                    <iframe src={gformLink} width="100%" height="750"></iframe>
                     <Button onClick={finishTest} className="btn btn-success">
                       Finish
                     </Button>
