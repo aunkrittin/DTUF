b4e25945fe9bd6ab46b85391f1881a29ff564eab
Krittin <macchiatolive@gmail.com>
2022-08-18 17:43:04 +0700

    Create ScreenCapture.js

---
+++ src/components/ScreenCapture.js
@@ -0,0 +1,10 @@
+// const startCapture = (displayMediaOptions) => {
+//   return navigator.mediaDevices
+//     .getDisplayMedia(displayMediaOptions)
+//     .catch((err) => {
+//       console.error(`Error:${err}`);
+//       return null;
+//     });
+// };
+
+// export default startCapture;
