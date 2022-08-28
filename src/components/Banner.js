829a8615cd4bb78f6425f870b785125db5dc1cff
Krittin <macchiatolive@gmail.com>
2022-08-28 16:42:41 +0700

    banner: add condition when press button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../assets/Banner.css";
-import { Link } from "react-router-dom";
+import { AuthContext } from "./Auth";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function Banner() {
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
   let bannerData = {
     title: "Don't turn your face!!",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similiqueperspiciatis iste porro sed, repellendus odit earum accusamus ametsequi, tempore consequatur culpa laboriosam quod! Obcaecati abreprehenderit quo ut at",
   };
+
+  function checkCurrentUser() {
+    // console.log("asdasdasdas");
+    if (currentUser) {
+      // console.log("currentUser: " + currentUser.name);
+      navigate("/teacher", { replace: true });
+    } else {
+      navigate("/signup", { replace: true });
+    }
+  }
+
   return (
     <div className="banner-bg">
       <div className="container">
@@ -14,9 +29,9 @@ function Banner() {
           <div className="banner-text">
             <h1>{bannerData.title}</h1>
             <p>{bannerData.desc}</p>
-            <Link className="banner-btn" to="/signup">
+            <Button className="banner-btn" onClick={checkCurrentUser}>
               Get start with DTUF
-            </Link>
+            </Button>
           </div>
         </div>
       </div>
