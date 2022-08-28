73595e0b921c6c3769270baab96d8719980e7613
Krittin <macchiatolive@gmail.com>
2022-08-28 18:00:35 +0700

    pages(content): optimize code

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,12 +7,12 @@ function Content() {
       <section className="content-con">
         <div className="content-l">
           <img
-            src="https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/296472945_412408587655324_8288286767833722980_n.png?_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeFthDK3sd7eqqD8XvuELH8xJ13giCfSm_UnXeCIJ9Kb9WS432WmS7Cady0Yio2AVm7UHoew5CRXDZj7_LV9UtEq&_nc_ohc=ZYG3o65f3T0AX8ZdQiL&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJLaMWf99GfVKl7P_1GLQKlRFiVvVKxYHvudMk7fQOjDQ&oe=6331ADCB"
             alt=""
           />
         </div>
         <div className="content-r">
-          <h2>Some title</h2>
+          <h2>Forward</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
             perspiciatis iste porro sed, repellendus odit earum accusamus amet
@@ -23,7 +23,7 @@ function Content() {
       </section>
       <section className="content-con">
         <div className="content-r">
-          <h2>Some title</h2>
+          <h2>Turn Right</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
             perspiciatis iste porro sed, repellendus odit earum accusamus amet
@@ -33,7 +33,7 @@ function Content() {
         </div>
         <div className="content-l">
           <img
-            src="https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/293118684_6164406746908655_5382650077510319963_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeHGKkfwRQ9CXkNrBzklvq_nbsg4ViwqtOVuyDhWLCq05XSX5PtpVBbe22dk4ljoF2xBUa1mNcrLMzaTiEry9pL8&_nc_ohc=PovghUNeucgAX-Dn9Lc&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJeJgj1uX5wYMF_EbkXK3NFlZQz6t6A8q0GE7cLg5GgMA&oe=6332A411"
             alt=""
           />
         </div>
@@ -41,12 +41,12 @@ function Content() {
       <section className="content-con">
         <div className="content-l">
           <img
-            src="https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/298111585_834088820909768_1901015362743327104_n.png?_nc_cat=108&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeFdbf6Ff7OzGd3BkIJSwBkOnSxNXFtCQg6dLE1cW0JCDmcf8BdGMn96SgKedV7szbCo5ijOP_1HzHI2dXPZBtQS&_nc_ohc=SMW292YbhH8AX8sm8ak&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJtEDtyXVKb1w7jgftEOYn8Jnj1dirRm31GaPQkcylisw&oe=63326566"
             alt=""
           />
         </div>
         <div className="content-r">
-          <h2>Some title</h2>
+          <h2>Turn Left</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
             perspiciatis iste porro sed, repellendus odit earum accusamus amet
