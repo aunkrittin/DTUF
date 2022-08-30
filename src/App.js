e7357e6df264dcd2479c69e8a0f8877cbbce6ecf
icutmyhair_ <macchiatolive@gmail.com>
2022-08-30 21:19:17 +0700

    update page not found

--- src/App.js
+++ src/App.js
@@ -12,31 +12,35 @@ import Details from "./pages/Details";
 import Evidences from "./pages/Evidences";
 import Resetpassword from "./components/Resetpassword";
 import { AuthProvider } from "./components/Auth";
+import NotMatch from "./components/NotMatch";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <AuthProvider>
-        <Router>
-          <NavBar />
-          <Routes style={{ height: "500px" }}>
-            {/* <Route path="/" element={<Home />} /> */}
-            {/* <Route path="/forms" element={<Forms />} /> */}
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/student/:handle" element={<Student />} />
-            <Route path="/teacher" element={<Teacher />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/resetpassword" element={<Resetpassword />} />
-            <Route path="/details/:handle" element={<Details />} />
-            <Route path="/details/:handle/:handle2" element={<Evidences />} />
-          </Routes>
-          {/* <Footer /> */}
-        </Router>
-      </AuthProvider>
-    </div>
+    <>
+      <div className="App">
+        <AuthProvider>
+          <Router>
+            <NavBar />
+            <Routes style={{ height: "500px" }}>
+              {/* <Route path="/" element={<Home />} /> */}
+              {/* <Route path="/forms" element={<Forms />} /> */}
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/student/:handle" element={<Student />} />
+              <Route path="/teacher" element={<Teacher />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/resetpassword" element={<Resetpassword />} />
+              <Route path="/details/:handle" element={<Details />} />
+              <Route path="/details/:handle/:handle2" element={<Evidences />} />
+              <Route path="*" element={<NotMatch />} />
+            </Routes>
+            {/* <Footer /> */}
+          </Router>
+        </AuthProvider>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/NotMatch.js
@@ -0,0 +1,16 @@
+function NotMatch() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      Page not found
+    </div>
+  );
+}
+
+export default NotMatch;
